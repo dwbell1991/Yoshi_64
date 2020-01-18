bccdd5eb482fa8d4cecfca9ebde2f4ebbb72122e
dwbell1991 <dwbell1991@gmail.com>
2020-01-17 20:11:12 -0600

    Yoshi_64 Unity game

---
+++ Yoshi_64/Assets/MoveHead.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHead : MonoBehaviour {
+
+	public float speed = 2f;
+	public float maxRotation = 15f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(maxRotation * Mathf.Sin(Time.time * speed), 0f, 0f);
+	}
+}
---
+++ Yoshi_64/Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Animator anim;
+	private CharacterController controller;
+	public float speed = 6.0f;
+	public float turnSpeed = 60.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	public float gravity = 20.0f;
+	private int jumping = 0;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponentInChildren<Animator>();
+		controller = GetComponent <CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (controller.isGrounded) {
+			jumping = 0;
+		}
+
+		if (Input.GetKey ("up") || Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("down")) {
+			anim.SetInteger ("AnimParam", 1);
+		} else {
+			anim.SetInteger ("AnimParam", 0);
+		}
+
+		if (Input.GetKey ("t")) {
+			transform.position = new Vector3(72.5f, 186.2f, -49.4f);
+		}
+			
+		if (Input.GetKeyDown ("space")) {
+			jumping = 1;
+
+		}
+		if (jumping == 1) {
+			transform.Translate (Vector3.up * 35 * Time.deltaTime, Space.World);
+		}
+
+		if (controller.isGrounded) {
+			moveDirection = -1 * transform.forward * Input.GetAxis ("Vertical") * speed;
+		}
+
+		float turn = Input.GetAxis ("Horizontal");
+		transform.Rotate (0, turn * turnSpeed * Time.deltaTime, 0);
+		controller.Move (moveDirection * Time.deltaTime);
+		moveDirection.y -= gravity * Time.deltaTime;
+	}
+}
